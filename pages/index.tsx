9df793d623708ccc054058bc4257c42bfa6af10a
colerbaba1337 <105028889+colerbaba1337@users.noreply.github.com>
2024-01-03 17:44:27 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,28 @@
+app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
 
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
 
+export default Home
